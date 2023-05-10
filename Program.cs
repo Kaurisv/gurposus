4fb2a9b2fd9ba989c7a3e3610c72332abeca5a71
Kaurisv <cam0033306@est.univalle.edu>
2023-05-10 14:16:29 -0400

    mi primer hola mundo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ejericio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("holaisss");
+            int n1=3; int n2=3;
+            int suma = n1 + n2;
+            Console.WriteLine(suma);
+
+
+
+        }
+    }
+}
